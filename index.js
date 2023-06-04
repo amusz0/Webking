0652ac4bc7f077b2841192be5aae2a6de39b5676
Franky Amu <89007800+amusz0@users.noreply.github.com>
2023-06-04 15:39:49 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const AMAZON = {
+    clientID: "",
+    clientSecret: ""
+};
+const GITHUB = {
+    clientID: "d3b2e82330dc5b49d33f",
+    clientSecret: "dc7973ead0476c3702b84c099145bf8c0dc3f4fa"
+};
+const FACEBOOK = {
+    clientID: "797141601984670",
+    clientSecret: "49a2e3057451dca2185e048c938f6a3d"
+};
+const GOOGLE = {
+    clientID: "1007176079036-a3np4e3ddeb1o06umo682gh3jtsijvo9.apps.googleusercontent.com",
+    clientSecret: "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"
+};
+const INSTAGRAM = {
+    clientID: "",
+    clientSecret: ""
+};
+const SPOTIFY = {
+    clientID: "",
+    clientSecret: ""
+};
+const TWITCH = {
+    clientID: "",
+    clientSecret: ""
+};
+
+module.exports = {
+    AMAZON,
+    GITHUB,
+    FACEBOOK,
+    GOOGLE,
+    INSTAGRAM,
+    SPOTIFY,
+    TWITCH
+};
